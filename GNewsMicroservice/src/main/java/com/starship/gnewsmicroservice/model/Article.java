f3ae4de705ee66bb6351d7fd4804f31409ea2d0f
kayatsa <kayatsa8@gmail.com>
2024-01-28 14:10:32 +0200

    GNewsMicroService: using lombok

--- GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/model/Article.java
+++ GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/model/Article.java
@@ -3,10 +3,14 @@ package com.starship.gnewsmicroservice.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Document("Articles")
 public class Article {
 
@@ -36,65 +40,5 @@ public class Article {
         this.source = source;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getPublishedAt() {
-        return publishedAt;
-    }
-
-    public void setPublishedAt(String publishedAt) {
-        this.publishedAt = publishedAt;
-    }
-
-    public Source getSource() {
-        return source;
-    }
-
-    public void setSource(Source source) {
-        this.source = source;
-    }
-    public LocalDateTime getAddedAt() {
-        return addedAt;
-    }
-    public void SetAddedAt(LocalDateTime addedAt) {
-        this.addedAt = addedAt;
-    }
 }
+
