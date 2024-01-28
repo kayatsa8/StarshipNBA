046f10570af9be983888679a0bd8480879596803
kayatsa <kayatsa8@gmail.com>
2024-01-28 10:16:40 +0200

    APIGateway: changed group name to "starship"

--- APIGateway/src/main/java/com/starship/apigateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/starship/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.gateway.apigateway;
+package com.starship.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
