573e270a595b04147d3329c85abdce53d9410d78
kayatsa <kayatsa8@gmail.com>
2024-01-31 08:08:51 +0200

    GNewsMicroservice: logger (log4j) added to all classes. old Log deprecated.

--- GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/controller/GNewsController.java
+++ GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/controller/GNewsController.java
@@ -4,6 +4,8 @@ import com.starship.gnewsmicroservice.log.Log;
 import com.starship.gnewsmicroservice.model.Article;
 import com.starship.gnewsmicroservice.service.ArticleService;
 import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,32 +21,48 @@ public class GNewsController {
 
     private ArticleService service;
 
+    private static Logger logger = LogManager.getLogger(GNewsController.class);
+
 
     public GNewsController(ArticleService articleService){
         service = articleService;
+
+        logger.info("GNewsController was created successfully");
     }
 
     @Scheduled(cron = "0 0 8-20 * * ?")
     public void autoImportNews(){
-        Log.info("importing articles");
-        System.out.println("importing articles");
+        logger.debug("Scheduled article import");
+
         service.init();
 
+        logger.debug("Articles imported");
     }
     @PostConstruct
     public void init(){
+        logger.info("GNewsController init");
         autoImportNews();
     }
 
     @GetMapping("/top-headlines")
     public List<Article> getTopHeadlines(){
         LocalDateTime fiveHoursAgo = LocalDateTime.now().minusHours(5);
-        Log.info("searching for articles since " + fiveHoursAgo);
+        logger.debug("searching for articles since " + fiveHoursAgo);
         return service.findAllByAddedAtAfter(fiveHoursAgo);
     }
 
     @PostMapping("/top-headlines")
     public void importTopHeadLines(){
+        logger.debug("manually import top-headlines");
         service.init();
     }
+
+
+//    @GetMapping("/log")
+//    public String logTest(){
+//        logger.debug("Debug log message");
+//        logger.info("Info log message");
+//        logger.error("Error log message");
+//        return "good";
+//    }
 }
--- GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/log/Log.java
+++ GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/log/Log.java
@@ -5,43 +5,44 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+@Deprecated
 public class Log {
 
-    private static Logger logger = null;
-
-    private static void init(){
-        if(logger == null){
-            logger = Logger.getLogger("Logger");
-            logger.setUseParentHandlers(false);
-
-            try {
-                FileHandler handler = new FileHandler("log_news", true);
-                logger.addHandler(handler);
-                SimpleFormatter formatter = new SimpleFormatter();
-                handler.setFormatter(formatter);
-            }
-            catch (SecurityException | IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    private static Logger logger = null;
+//
+//    private static void init(){
+//        if(logger == null){
+//            logger = Logger.getLogger("Logger");
+//            logger.setUseParentHandlers(false);
+//
+//            try {
+//                FileHandler handler = new FileHandler("log_news", true);
+//                logger.addHandler(handler);
+//                SimpleFormatter formatter = new SimpleFormatter();
+//                handler.setFormatter(formatter);
+//            }
+//            catch (SecurityException | IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     public static void info(String message){
-        init();
-
-        logger.info(message);
+//        init();
+//
+//        logger.info(message);
     }
 
     public static void warn(String message){
-        init();
-
-        logger.warning(message);
+//        init();
+//
+//        logger.warning(message);
     }
 
     public static void severe(String message){
-        init();
-
-        logger.severe(message);
+//        init();
+//
+//        logger.severe(message);
     }
 
 
