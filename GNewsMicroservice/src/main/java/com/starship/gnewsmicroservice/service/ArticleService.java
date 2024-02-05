30ca405caa4f896a5082df9e3dc19deb51feebe9
kayatsa <kayatsa8@gmail.com>
2024-02-05 12:32:51 +0200

    ArticleAService -> ArticleServiceImpl added ArticleService interface. GNewsController uses ArticleService.

--- GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/service/ArticleService.java
+++ GNewsMicroservice/src/main/java/com/starship/gnewsmicroservice/service/ArticleService.java
@@ -1,49 +1,16 @@
 package com.starship.gnewsmicroservice.service;
 
 import com.starship.gnewsmicroservice.model.Article;
-import com.starship.gnewsmicroservice.repository.ArticleRepository;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.starship.commons.Fetcher;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 
-@Service
-public class ArticleService {
+public interface ArticleService {
 
-    private static Logger logger = LogManager.getLogger(ArticleService.class);
+    void init();
 
-    private final ArticleRepository repository;
+    List<Article> findAll();
 
+    List<Article> findAllByAddedAtAfter(LocalDateTime publishDate);
 
-    public ArticleService(ArticleRepository repo){
-        repository = repo;
-
-        logger.info("ArticleService was created");
-    }
-
-    public void init() {
-        logger.debug("ArticleService fetching articles from api");
-
-        new Fetcher<>(repository)
-                .fetch("https://gnews.io/api/v4/top-headlines?country=il&category=general" +
-                        "&apikey=ca6803dbe05ebcfd71267c4e89437524", Article.class,
-                        new HashMap<>(), (root) -> root.path("articles"));
-
-        logger.debug("Articles fetched successfully");
-    }
-
-    public List<Article> findAll(){
-        logger.debug("ArticleService find all articles");
-        return repository.findAll();
-    }
-
-    public List<Article> findAllByAddedAtAfter(LocalDateTime publishDate){
-        logger.debug("ArticleService find all articles after given date");
-        return repository.findAllByAddedAtAfter(publishDate);
-    }
 }
