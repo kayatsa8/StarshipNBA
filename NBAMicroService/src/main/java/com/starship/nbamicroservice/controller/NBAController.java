62c681c882f84d2672618281de47361c22db023a
Aviv Menahem <88618518+avivaws@users.noreply.github.com>
2024-02-24 21:33:04 +0200

    fixes to zipkin

--- NBAMicroService/src/main/java/com/starship/nbamicroservice/controller/NBAController.java
+++ NBAMicroService/src/main/java/com/starship/nbamicroservice/controller/NBAController.java
@@ -11,6 +11,8 @@ import jakarta.annotation.PostConstruct;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,8 @@ public class NBAController {
     private MeterRegistry registery;
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
 
 
@@ -85,7 +89,12 @@ public class NBAController {
     @GetMapping("/news/top-headlines")
     public String getTopHeadlines(){
         logger.info("fetching top-headlines");
-        return restTemplate.getForObject("http://ms-news/api/news/top-headlines", String.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("ms-news");
+        if(instances.isEmpty()){
+            logger.error("ms-news service not found");
+            return "ms-news service not found";
+        }
+        return restTemplate.getForObject(instances.get(0).getUri()+"/api/news/top-headlines", String.class);
     }
 
 
